23b62ce608dab25ad163ad013f4924c690ebaf94
Sanket Kankarej <sanket.kankarej2010@gmail.com>
2019-12-27 17:34:35 +0530

    Removed transactions for find query from class

--- routes/class/list.js
+++ routes/class/list.js
@@ -8,29 +8,18 @@ const Response = require('@globals/response');
 
 const router = express.Router();
 
-router.post('/list', async function (request, response) {
+router.post('/list', function (request, response) {
     // Prepare
     const query = {
         department: request.body.department
     };
 
-    // Session
-    const session = await Class.startSession();
-    session.startTransaction();
-
     // Run
     Class.find(query, function (error, list) {
         if (error) {
             console.error(error);
-
-            session.abortTransaction();
-            session.endSession();
-
             new Response(response, 400, null, null);
         } else {
-            session.commitTransaction();
-            session.endSession();
-            
             new Response(response, 200, null, list);
         }
     });
