31bd2a879d53a9ceac0b10a69c046307c2be9024
Sanket Kankarej <sanket.kankarej2010@gmail.com>
2019-12-28 12:54:22 +0530

    Modified list route to fetch subject list from semester

--- routes/subject/list.js
+++ routes/subject/list.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 // Models
 const Class = require('@models/class');
+const Semester = require('@models/semester');
 
 // Globals
 const Response = require('@globals/response');
@@ -10,15 +11,27 @@ const router = express.Router();
 
 router.post('/list', function (_, response) {
     // Prepare
-    const query = {};
+    let query = {};
 
     // Run
-    Class.findOne(query).populate('subjects').exec(function (error, _class) {
+    Class.findOne(query, function (error, _class) {
         if (error) {
             console.error(error);
             new Response(response, 400, null, null);
         } else {
-            new Response(response, 200, null, _class.subjects);
+            // Prepare
+            query = {
+                _id: _class.semester
+            };
+
+            // Run
+            Semester.findOne(query).populate('subjects').exec(function (error, subjects) {
+                if(error) {
+                    console.error(error);
+                } else {
+                    new Response(response, 200, null, subjects);
+                }
+            })
         }
     });
 });
